ed76f483386bddee808525cddb85434c5c434c62
zengchao <359921330@qq.com>
2025-08-15 22:38:11 +0800

    增加镜像构建脚本文件

---
+++ docker-build.sh
@@ -0,0 +1,6 @@
+VERSION="1.0.0"
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg VERSION=${VERSION} -t zengchao0215/gpu-calculator:${VERSION} --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t zengchao0215/gpu-calculator:latest --push .
+VERSION="0.0.3"
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg VERSION=${VERSION} -t registry.riilservice.cn/zengchao0215/gpu-calculator:${VERSION} --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t registry.riilservice.cn/zengchao0215/gpu-calculator:latest --push .
